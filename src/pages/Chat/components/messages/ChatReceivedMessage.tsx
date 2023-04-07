6ffb3c0fe5ef78a36a821d4ea2159965fbd3273f
yershhov <yershhov@gmail.com>
2023-04-08 01:47:34 +0200

    added timestamp for received messages

--- src/pages/Chat/components/messages/ChatReceivedMessage.tsx
+++ src/pages/Chat/components/messages/ChatReceivedMessage.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { Message } from "../../../../firebase/entities/message";
+
 type ReceivedMessageProps = {
   message: Message;
 };
@@ -7,6 +7,19 @@ type ReceivedMessageProps = {
 const ReceivedMessage = (props: ReceivedMessageProps) => {
   return (
     <div className="chat chat-start">
+      <div className="chat-header">
+        <time className="text-xs opacity-50">
+          {props.message.sentAt
+            //@ts-ignore
+            .toDate()
+            .toLocaleTimeString(window.navigator.language, {
+              day: "numeric",
+              month: "short",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+        </time>
+      </div>
       <div className="chat-bubble dark:bg-lightDark dark:text-gray-200">
         {props.message.text}
       </div>
