d2232efed603c169f6154eb8986a3478371d1d96
yershhov <yershhov@gmail.com>
2023-04-09 14:14:12 +0200

    fixed a bug of non dissapearing search result cards

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,6 +1,8 @@
+import { uuidv4 } from '@firebase/util';
 import { collection, doc, getDocs, Query, query, QueryDocumentSnapshot, where } from 'firebase/firestore';
 import { motion } from 'framer-motion'
 import React, { ChangeEvent, createContext, useCallback, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import { firestore } from '../../firebase/config';
 import { userConverter, UserEntity } from '../../firebase/entities/user';
 import SearchHeader from './SearchHeader'
@@ -19,7 +21,7 @@ export const SearchContext = createContext<SearchConxtxtModel>(searchInitialStat
 const Search = () => {
     const [search, setSearch] = useState('')
     const [allUsers, setAllUsers] = useState<QueryDocumentSnapshot<UserEntity>[]>([])
-    const [searchData, setSearchData] = useState<string[]>([])
+    const [searchData, setSearchData] = useState<QueryDocumentSnapshot<UserEntity>[]>([])
 
     const handleSearchChange = (e: ChangeEvent) => {
         setSearch((e.target as HTMLInputElement).value)
@@ -39,28 +41,35 @@ const Search = () => {
 
     useEffect(() => {
         if (search !== '') {
-            const filtered = allUsers.map(user => {
+            const filtered = allUsers.filter(user => {
                 const userEmail = user.data().email
                 if (userEmail.includes(search)) {
                     return userEmail
                 }
             })
-            // @ts-ignore
-            setSearchData(filtered)
-        }
-    }, [search])
 
+            setSearchData(filtered)
+        } else setSearchData([])
+    }, [search])
     return (
         <motion.div className="h-[calc(100%-3.8rem)]">
             <SearchContext.Provider value={{ search, handleSearchChange }}>
                 <SearchHeader />
                 <div className="h-full overflow-y-auto">
-
-                    {searchData.map(email => <div>{email}</div>
-                    )}
+                    {searchData.map(user => <Link to={`/${user.data().email}`}>
+                        <div
+                            key={uuidv4()}
+                            className=" border-b dark:border-zinc-700 flex items-center gap-4 px-4 py-1 hover:cursor-pointer hover:backdrop-brightness-150"
+                        >
+                            <div className="h-[3rem] dark:bg-zinc-700 aspect-square rounded-full"></div>
+                            <div className="flex flex-col gap-2">
+                                <p className="font-semibold text-[12px]">{user.data().email}</p>
+                            </div>
+                        </div>
+                    </Link>)}
                 </div>
             </SearchContext.Provider>
-        </motion.div>
+        </motion.div >
 
     )
 }
