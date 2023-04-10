8d400ec3569ed4bdb940990f505d29b996a08aac
yershhov <yershhov@gmail.com>
2023-04-10 02:56:03 +0200

    created welcome screnn, to adjust for mobile

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -20,32 +20,48 @@ const SignIn = () => {
   }, []);
 
   return (
-    <div className="grid place-items-center h-full">
-      <button
-        ref={authButtonRef}
-        type="button"
-        className="text-white bg-primary hover:bg-primary/90 focus:ring-4 focus:outline-none
-         focus:ring-primary/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
-         inline-flex items-center dark:focus:ring-primary/55"
-        onClick={signInWIthGoogle}
-      >
-        <svg
-          className="w-4 h-4 mr-2 -ml-1"
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fab"
-          data-icon="google"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 488 512"
-        >
-          <path
-            fill="currentColor"
-            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-          ></path>
-        </svg>
-        Sign in with Google
-      </button>
+    <div className=" h-full">
+      <div className=" h-full text-center flex flex-col px-[8rem] sm:px-7 gap-24">
+        <div className="grid gap-7 w-full pt-20">
+          <h1 className="text-4xl font-black text-raisinBlack">WELCOME!👋</h1>
+          <div className="font-semibold text-left text-gray-500 text-[12px] leading-[1.1rem] sm:leading-[0.9rem] w-full px-4">
+            <h3 >😊A couple things to know:</h3>
+            <br />
+            <p >1. For now the app only supports <br /> authentication with gmail account, <br />other options will be added soon!</p>
+            <br />
+            <p>2. Smooth animations, beautiful <br /> loading states and performance <br /> enchancements to be done <br /> in the nearest future🙉</p>
+            <br />
+            <p>3. Feel free to report any bugs, <br /> ask questions, suggest new features,<br />  etc. on <a href="mailto:yershhov@gmail.com" className="inline text-white/50 font-black hover:brightness-90">yershhov@gmail.com</a> <br /> or directly in this app <br /> (find me by the same email)</p>
+          </div>
+        </div>
+        <div className="w-full px-4">
+          <button
+            ref={authButtonRef}
+            type="button"
+            className="text-white bg-primary hover:bg-primary/90 focus:ring-4 focus:outline-none
+        focus:ring-primary/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+        inline-flex items-center justify-center dark:focus:ring-primary/55 w-full"
+            onClick={signInWIthGoogle}
+          >
+            <svg
+              className="w-4 h-4 mr-2 -ml-1"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="google"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 488 512"
+            >
+              <path
+                fill="currentColor"
+                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+              ></path>
+            </svg>
+            Sign in with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
